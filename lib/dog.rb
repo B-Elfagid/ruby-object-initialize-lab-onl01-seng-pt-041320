05da6d7f7d634b34422d64af25977f8cec4602f4
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-04-30 13:07:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,19 @@
-class Dog 
-  def initialize(name, breed = "Mutt")
-    @name = name 
-    @breed = breed
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
   end 
 end 
-
-def name
+  
